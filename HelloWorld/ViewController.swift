04841fbf63e8f7049de4599ae3be9f70d057d194
Eason <icecomic5566@livemail.tw>
2020-06-12 00:23:01 +0800

    init commit

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    let emojiName = ["有著愛心的微笑表情", "喜極而泣的表情", "眨眼伸舌頭的表情", "冷笑的表情"]
+    
+    // MARK: - IBAction
+    
+    @IBAction func emojiPressed(_ sender: UIButton) {
+        
+        switch sender.tag {
+        case 0:
+            showAlertView(by: sender.titleLabel!.text!, with: emojiName[0])
+        case 1:
+            showAlertView(by: sender.titleLabel!.text!, with: emojiName[1])
+        case 2:
+            showAlertView(by: sender.titleLabel!.text!, with: emojiName[2])
+        case 3:
+            showAlertView(by: sender.titleLabel!.text!, with: emojiName[3])
+        default:
+            break
+        }
+    }
+    
+    
+    // MARK: - View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    // MARK: - Custom Function
+    
+    func showAlertView(by title: String, with description: String) {
+        
+        let alertControlller = UIAlertController(title: " \(title) 表示\(description)", message: nil, preferredStyle: .alert)
+        alertControlller.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertControlller, animated: true, completion: nil)
+    }
 }
 
